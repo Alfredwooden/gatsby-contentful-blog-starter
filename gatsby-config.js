d7105d443e5ee9524382c4779e215ead9aa88f96
Alfredwooden <coomcs@gmail.com>
2020-03-02 08:34:29 -0300

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `My Awesome Blog`,
+    description: `An awesome blog displaying my awesome posts.`,
+    author: `Alfredo Barros`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -13,6 +13,7 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
@@ -25,6 +26,13 @@ module.exports = {
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `mdy1v6002qwa`, // YOUR_SPACE_ID
+        accessToken: `wCZH1vlSJkvGgVcbSftJR2X4oS-0ckuwok4w5Xaz9Cw`, // YOUR_CONTENT_DELIVERY_API_KEY
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
